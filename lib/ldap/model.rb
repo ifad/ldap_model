a058963a437f0504e526a053e41fc7922b46631b
Marcello Barnaba <vjt@openssl.it>
2013-04-03 16:34:43 +0200

    Add Railtie

--- lib/ldap/model.rb
+++ lib/ldap/model.rb
@@ -1,6 +1,10 @@
 require 'ldap/model/version'
 require 'ldap/model/base'
 
+if defined?(Rails)
+  require 'ldap/model/railtie'
+end
+
 if defined?(::Hirb)
   ::Hirb.add_dynamic_view('LDAP::Base', :helper => :auto_table) do |obj|
     {:fields => obj.class.string_attributes}
---
+++ lib/ldap/model/railtie.rb
@@ -0,0 +1,27 @@
+module LDAP::Model
+  class Railtie < ::Rails::Railtie
+    console do |app|
+      LDAP::Model::Base.logger = Logger.new(STDERR)
+    end
+
+    initializer 'ldap_model.logger' do
+      LDAP::Model::Base.logger ||= ::Rails.logger
+    end
+
+    initializer 'ldap_model.active_record' do
+      require 'ldap/model/active_record'
+      ActiveSupport.on_load(:active_record) do
+        extend LDAP::Model::ActiveRecord
+      end
+    end
+
+    initializer 'ldap_model.log_runtime' do
+      require 'ldap/model/instrumentation'
+      LDAP::Model::Instrumentation::LogSubscriber.attach_to :ldap
+
+      ActiveSupport.on_load(:action_controller) do
+        include LDAP::Model::Instrumentation::ControllerRuntime
+      end
+    end
+  end
+end
