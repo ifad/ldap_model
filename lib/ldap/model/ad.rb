1c5e0f0d5200e473b5be495944836b178836ac54
Marcello Barnaba <vjt@openssl.it>
2013-04-04 16:55:49 +0200

    Fix AD time interval parsing

--- lib/ldap/model/ad.rb
+++ lib/ldap/model/ad.rb
@@ -1,17 +1,23 @@
+require 'active_support/core_ext/time/zones'
+
 module LDAP::Model
   module AD
 
     # Difference in seconds between the UNIX Epoch (1970-01-01)
     # and the Active Directory Epoch (1601-01-01)
-    EPOCH_OFFSET = 11644477200
+    EPOCH_OFFSET = Time.utc(1601).to_i
     def self.now
-      (Time.now.to_i + EPOCH_OFFSET) * 10_000_000
+      ((utc.now.to_f - EPOCH_OFFSET) * 10_000_000).to_i
     end
 
     # number of nanosec / 100, i.e. 10 times the number of microsec,
     # divide by 10_000_000 so it becomes number of seconds
     def self.at(timestamp)
-      Time.at(timestamp / 10_000_000 - EPOCH_OFFSET)
+      utc.at(timestamp / 10_000_000.0 + EPOCH_OFFSET).localtime
+    end
+
+    def self.utc
+      @utc ||= Time.find_zone('UTC')
     end
 
   end
